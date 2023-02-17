90160a9cf84718d52e624ec4478d4a592b97e08a
JORDAM MENDES <91645575+jordam1407@users.noreply.github.com>
2023-02-17 16:12:16 -0300

    chore: correcting tailwind config/ feat: chat.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Chat from './components/Chat';
 
 function App() {
+  const [data, setData] = useState([]);
+  const items = data.map((item) => item.id);
+  console.log('itemsssss', items);
+
+  useEffect(() => {
+    const getLogin = async () => {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        params: data,
+      };
+
+      try {
+        const response = await axios.get(
+          'http://localhost:3001/search',
+          config,
+        );
+        setData(response.data);
+        console.log(response.data[0].id);
+      } catch (error) {
+        return console.log(error.message);
+      }
+    };
+    getLogin();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>BASIC PROJECT STRUCTURE</h1>
-      <h2>PJ TECH SOLUTIONS</h2>
+    <div className="border-2">
+      <h1 className="text-5xl font-bold underline">Chat Bot</h1>
+      <ul>
+        {data.map((i, index) => <li key={ index }>{`${i.id} ${i.titulo}`}</li>)}
+      </ul>
+      <Chat />
     </div>
   );
 }
