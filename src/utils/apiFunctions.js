1c8f50a9c36bfb080f5b997cdd07edfb53bada45
JORDAM MENDES <91645575+jordam1407@users.noreply.github.com>
2023-02-22 10:31:24 -0300

    feat: adding getSteps function

--- src/utils/apiFunctions.js
+++ src/utils/apiFunctions.js
@@ -1,14 +1,22 @@
 import axios from 'axios';
 
-const URL_STEPS = 'http://localhost:3001/steps';
 const getTitles = async () => {
+  const URL_TITLES = 'http://localhost:3001/steps';
+  try {
+    const response = await axios.get(URL_TITLES);
+    return response.data;
+  } catch (error) {
+    return console.log(error.message);
+  }
+};
+const getSteps = async (id) => {
+  const URL_STEPS = `http://localhost:3001/steps/${id}`;
   try {
     const response = await axios.get(URL_STEPS);
-    console.log(response.data[0].id);
     return response.data;
   } catch (error) {
     return console.log(error.message);
   }
 };
 
-export default getTitles;
+export { getTitles, getSteps };
